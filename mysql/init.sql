ff5428495b69792cc1bc8bda7433e798fd1ec458
rorik <arsyred@gmail.com>
2025-12-09 21:12:40 +0000

    Initial VDI project setup

---
+++ mysql/init.sql
@@ -0,0 +1,5 @@
+-- Создание таблиц для Guacamole
+CREATE DATABASE IF NOT EXISTS guacamole_db;
+CREATE USER IF NOT EXISTS 'guacamole_user'@'%' IDENTIFIED BY 'guacamole_password';
+GRANT ALL PRIVILEGES ON guacamole_db.* TO 'guacamole_user'@'%';
+FLUSH PRIVILEGES;
